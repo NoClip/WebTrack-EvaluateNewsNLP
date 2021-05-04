fa146a65637048026898a7f4e964844e83a37ac5
QWERTY <bassam.mh@gmail.com>
2021-05-05 00:40:39 +0200

    added endpoint sentiment to server

--- src/server/index.js
+++ src/server/index.js
@@ -1,34 +1,49 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-
-const app = express()
-
+var path = require('path');
+const express = require('express');
+//const mockAPIResponse = require('./mockAPI.js');
 const dotenv = require('dotenv');
-const result = dotenv.config();
+const fetch = require('node-fetch');
 
+const app = express();
+app.use(express.static('dist'));
+
+const result = dotenv.config();
 if (result.error) {
     throw result.error;
 }
 
-// console.log(result.parsed);
-// console.log(`Your API key is ${process.env.API_KEY}`);
-
-
-app.use(express.static('dist'))
-
-console.log(__dirname)
+const apiKey = process.env.API_KEY;
+const baseUrl = `https://api.meaningcloud.com/sentiment-2.1?lang=en&key=${apiKey}`;
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
-})
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
-})
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+const getSentimentData = async (url = '') => {
+    const request = await fetch(url);
+
+    try {
+        // get data and return it
+        return await request.json();
+    }
+    catch (error) {
+        // if there is any error, return null
+        return null;
+    }
+};
+
+// sentiment endpoint with query parameter named: url
+// example: localhost:8081?url=http://www.example.com
+app.get('/sentiment', function (req, res) {
+    const url = `${baseUrl}&url=${req.query.url}`;
+
+    getSentimentData(url)
+        .then((sentimentResponse) => {
+            res.send(sentimentResponse);
+        });
+});
