476784b01c3312081aecf98d4a7e80e1adf675b7
QWERTY <bassam.mh@gmail.com>
2021-05-08 16:21:45 +0200

    added comment

--- src/server/__tests__/sentimentData.test.js
+++ src/server/__tests__/sentimentData.test.js
@@ -1,3 +1,7 @@
+// the below 2 imports for handling the error
+// ReferenceError: regeneratorRuntime is not defined
+// usually to use babel-polyfill but its deprecated
+// and using the new packages core-js/stable and regenerator-runtime/runtime
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import { getSentimentData } from '../app';
